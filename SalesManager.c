79166649120d69fd02729cbfb3262f645152e39e
Valdecir Raimundo <rm.valdecir@gmail.com>
2020-03-15 22:31:31 -0300

    adding postgres  db

--- SalesManager.c
+++ SalesManager.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <stdbool.h>
+#include <libpq-fe.h>
 
-#define STOCK_MAX_NUMBER_OF_RECORDS  500
-#define MAX_PRODUCT_NAME_SIZE  100
+#define STOCK_MAX_NUMBER_OF_RECORDS  100
+#define MAX_PRODUCT_NAME_LENGTH  100
+#define MAX_CONNECTION_STRING_LENGTH  100
 
 const float PROFIT_PERCENTAGE = 0.2;
 typedef enum {  Ok = 200, BadRequest = 400, NotFound = 404, InsufficientStorage = 507} ResultStatus;
+typedef enum { Purchase = 1, Sale = 2 } StockMovimentationType;
 
-typedef struct Product
+typedef struct
 {
-    int code;
+    int id;
+    char* code;
     char* name;
     float costPrice;
     float sellingPrice;
@@ -20,36 +25,33 @@ typedef struct Product
 
 typedef struct
 {
-    int id;
+    int id;     
+    int saleId;
+    int purchaseId;
     time_t date;
-    int totalQuantityProducts;
+    StockMovimentationType type;
+    int previousQuant;
+    int quant;
     double totalCost;
 
-}Sale;
+}StockMovimentation;
 
-typedef struct
+typedef struct StockMovimentation_Product
 {
+    int id;
+    StockMovimentation stockMovimentation;
     Product product;
-    Sale sale;
     int productQuantity;
     float productUnitaryPrice;
 
-}ProductSale;
+}SM_Product;
 
-typedef struct
+typedef struct 
 {
-    Product product;
-    int quantity;
+    Product products[STOCK_MAX_NUMBER_OF_RECORDS];
+    int lastIndex;
 
-}ProductRecord;
-
-typedef struct
-{
-    ProductRecord products[STOCK_MAX_NUMBER_OF_RECORDS]; 
-    int totalQuantity;
-
-}Stock; 
-
+}ProductsList;
 
 typedef struct
 {
@@ -58,7 +60,7 @@ typedef struct
 
 }Result;
 
-//=============================================================================================================
+//================================================= Helpers ============================================================
 
 void ClearBuffer()
 {
@@ -74,123 +76,119 @@ void RemoveNewLine(char *phase)
         phase[WordSize - 1] = '\0';
 }
 
-void SetSellingPrice(Product *product)
-{
-    float sellingPrice = product->costPrice + (product->costPrice*PROFIT_PERCENTAGE);
-    product -> sellingPrice = sellingPrice;
-}
+//=====================================================================================================================
 
-Result ProductIsRegistered(Product *product, Stock *stock)
-{
-    Result result = {"The Product is already registered in the stock", Ok};
-
-    for(int i=0; i < stock->totalQuantity; i++)    
-        if (stock->products[i].product.code == product->code)
-            return result;
-
-    result.message = "The Product is NOT registered in the stock";
-    result.status = NotFound;
-
-    return result;    
-}
-
-Result AddProductToStock(Product *product, Stock *stock, ProductRecord *pr, int prodQuant)
-{
-    Result result;
-
-    if (stock -> totalQuantity == STOCK_MAX_NUMBER_OF_RECORDS)
-    {
-        result.message = "Failed to add product, the stock is full";
-        result.status = InsufficientStorage;
-        return result;
-    }
-
-    result = ProductIsRegistered(product, stock);
-
-    if (result.status == Ok)
-    {
-        result.status = BadRequest;
-        return result;
-    }
-    pr -> product = *product;
-    pr -> quantity = prodQuant;
-
-    stock -> products[stock->totalQuantity] = *pr;
-    stock -> totalQuantity++;
-
-    result.message = "Product sucessfully added to stock.";
-    result.status = Ok;
-
-    return result;
-}
-
-
-Result RegisterProduct(Product *product, Stock *stock, ProductRecord *pr, char *name)
-{
-    int code, quantity;
-    float cost;
-
-    system("clear||cls");
-    printf("\n================================================\n\n");
-    printf("Inform the code of the product: ");
-    scanf("%d", &code);
-    printf("\nInform the name of the product: ");
-    ClearBuffer();
-    fgets(name, MAX_PRODUCT_NAME_SIZE, stdin);
-    RemoveNewLine(name);
-    printf("\nInform the quantity of products: ");
-    scanf("%d", &quantity);
-    printf("\nInform the purchase cost of the product: ");
-    scanf("%f", &cost);
-    printf("\n================================================\n");   
-
-    product -> code = code;
-    product -> name = name;
-    product -> costPrice = cost;
-
-    SetSellingPrice(product);
-
-    return AddProductToStock(product, stock, pr, quantity);
-}
-
-void ShowProductsStored(Result result, Stock stock)
-{
-    system("clear||cls");
-    for (int i = 0; i < stock.totalQuantity; i++)
-    {
-        printf("\n\n================================================"); 
-        printf("\n\n %s\n\n", result.message);
-        printf(" stored cost price: R$ %.2f\n", stock.products[i].product.costPrice);
-        printf(" stored seeling Price: R$ %.2f\n", stock.products[i].product.sellingPrice);
-        printf(" stored product name: %s\n", stock.products[i].product.name);
-        printf(" stored product code: %d\n", stock.products[i].product.code);
-        printf(" stored product quantity: %d\n", stock.products[i].quantity);
-        printf("\n================================================\n"); 
-    }    
-}
-
-int main ()
-{
-    int userOption;
-    Stock stock;
-    stock.totalQuantity = 0;
-
-    do
-    {
-        Product product;
-        ProductRecord pr;
-        char name[MAX_PRODUCT_NAME_SIZE];
-
-        Result result = RegisterProduct(&product, &stock, &pr, name);
-
-        ShowProductsStored(result, stock);
-
-        printf("Total quantity: %d\n", stock.totalQuantity);
-        printf("Do you want continue? (0 for exit and 1 to continue)\n");
-        scanf("%d", &userOption);
-
-    } while (userOption != 0);
+void CloseDbConnection(PGconn *conn, PGresult *res) {
     
+    char *errorMessage = PQerrorMessage(conn);
 
-    return 0; 
+    PQclear(res);
+    PQfinish(conn);    
+    
+    if (strlen(errorMessage) > 0)
+    {
+        fprintf(stderr, "%s\n", errorMessage);    
+        exit(1);
+    }
+}
+
+void SetConnectionString(char * connectionString)
+{
+    FILE *fp;
+    char fileName[] = "DbConnectionString.txt";
+
+    fp = fopen(fileName, "r");
+
+    if (fp == NULL)
+    {
+      printf("Error while opening the file: %s.\n", fileName);
+      exit(EXIT_FAILURE);
+    }
+    
+    fgets(connectionString, MAX_CONNECTION_STRING_LENGTH, fp);
+    fclose(fp);
+
+}
+
+PGresult* DbQuery(PGconn *conn, char* query)
+{
+    return PQexec(conn, query);
+}
+
+void PrintAllProducts(PGresult *res)
+{
+    int nRows = PQntuples(res);
+    int nCols = PQnfields(res);
+    
+    for(int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)                 
+            printf("%s  ", PQgetvalue(res, i, j));
+        printf("\n");
+    }
+}
+
+void PrintListProducts(ProductsList *productsList)
+{
+    for (int i = 0; i < productsList->lastIndex; i++)
+    {
+        printf("===================================================\n");
+        printf("%d  %s  %s  %2.f  %2.f\n", productsList->products[i].id, productsList->products[i].code, productsList->products[i].name, productsList->products[i].costPrice, productsList->products[i].sellingPrice); 
+        printf("===================================================\n\n");
+    }
+}
+
+void GetAllProducts(PGresult *res, ProductsList *productsList)
+{
+    int nRows = PQntuples(res);
+
+    Product product;
+    
+    for(int i = 0; i < nRows; i++)
+    {
+        product.code = PQgetvalue(res, i, 0);
+        product.costPrice = strtof(PQgetvalue(res, i, 1), NULL);
+        product.id = atoll(PQgetvalue(res, i, 2));
+        product.name = PQgetvalue(res, i, 3);
+        product.sellingPrice = strtof(PQgetvalue(res, i, 4), NULL);
+
+        productsList->products[productsList->lastIndex] = product;
+        productsList->lastIndex++;
+    }
+}
+
+int main() 
+{    
+    char connectionString[MAX_CONNECTION_STRING_LENGTH];    
+    SetConnectionString(connectionString);
+    ProductsList products;
+    products.lastIndex = 0;
+
+    PGconn *conn = PQconnectdb(connectionString);
+
+           
+    if (PQstatus(conn) == CONNECTION_BAD) 
+    {
+        fprintf(stderr, "Connection to database failed: %s\n",
+        PQerrorMessage(conn));
+
+        PQfinish(conn);
+        exit(1);
+    }
+   
+
+    PGresult *res = DbQuery(conn, "SELECT * FROM \"Product\"");
+
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+
+        printf("No data found\n");        
+        CloseDbConnection(conn, res);
+    } 
+
+    GetAllProducts(res, &products);
+    PrintListProducts(&products);
+
+    CloseDbConnection(conn, res);
+
+    return 0;
 }
